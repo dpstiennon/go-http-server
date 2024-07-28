be2fad128b53170cbaac55977ae3403d23b15535
David Stiennon <david.stiennon@gmail.com>
2024-07-27 22:59:09 -0500

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -18,9 +18,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	req, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	req.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
