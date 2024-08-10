f7f1de3dedba99802c59ae4cebbe2def9514a021
David Stiennon <david.stiennon@gmail.com>
2024-08-10 10:48:38 -0500

    made multithreaded!

--- app/server.go
+++ app/server.go
@@ -18,24 +18,28 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
 	}
 
+}
+
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
 	req := make([]byte, 1024)
-	conn.Read(req)
-	if err != nil {
-		fmt.Println("Error reading request:", err)
+	_, connErr := conn.Read(req)
+	if connErr != nil {
+		fmt.Println("Error reading request:", connErr)
 		return
 	}
 
@@ -53,8 +57,6 @@ func main() {
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-	conn.Close()
-
 }
 
 func simpleResponse(status int, content string) []byte {
