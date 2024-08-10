416ba2f469fbd21558955d44b07589fab58f0422
David Stiennon <david.stiennon@gmail.com>
2024-08-10 10:34:43 -0500

    add user agent endpoint

--- app/server.go
+++ app/server.go
@@ -10,8 +10,8 @@ import (
 
 func handleEcho(conn net.Conn, toEcho string) {
 	fmt.Println(toEcho)
-	resp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%v", len(toEcho), toEcho)
-	conn.Write([]byte(resp))
+	resp := simpleResponse(200, toEcho)
+	conn.Write(resp)
 }
 
 func main() {
@@ -39,7 +39,8 @@ func main() {
 		return
 	}
 
-	lines := strings.Split(string(req), "\n")
+	stringSplitRegex := regexp.MustCompile("(\r)?\n")
+	lines := stringSplitRegex.Split(string(req), -1)
 	urlLineParts := strings.Split(lines[0], " ")
 	path := urlLineParts[1]
 	echoRegex, _ := regexp.Compile("/echo/([^/]+)")
@@ -47,9 +48,28 @@ func main() {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	} else if matches := echoRegex.FindStringSubmatch(path); len(matches) > 1 && len(matches[1]) > 0 {
 		handleEcho(conn, matches[1])
+	} else if path == "/user-agent" {
+		handleUserAgent(conn, lines)
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
 	conn.Close()
 
+}
+
+func simpleResponse(status int, content string) []byte {
+	resp := fmt.Sprintf("HTTP/1.1 %d OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%v", status, len(content), content)
+	return []byte(resp)
+}
+
+func handleUserAgent(conn net.Conn, lines []string) {
+	var userAgent string
+	for _, line := range lines {
+		before, after, found := strings.Cut(line, " ")
+		if found && strings.ToLower(before) == "user-agent:" {
+			userAgent = after
+		}
+	}
+	resp := simpleResponse(200, userAgent)
+	conn.Write(resp)
 }
